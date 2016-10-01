662840f51b18d4255b5b68d2e44462a6a823810a
robinhodemorais <robinhodemorais@gmail.com>
2016-10-01 20:38:56 -0300

    Menu de navegação

--- dist/bill.component.js
+++ dist/bill.component.js
@@ -1,10 +1,10 @@
 "use strict";
 
 window.billComponent = Vue.extend({
-    template: "\n        <nav>\n            <ul>\n                <li v-for=\"o in menus\">\n                    <a v-link=\"{name: o.routeName}\">{{ o.name }}</a>\n                </li>\n            </ul>\n        </nav>\n        <router-view></router-view>\n        ",
+    template: "\n        <div class=\"navbar-fixed\">\n            <nav>\n                <div class=\"nav-wrapper container\">\n                            <a href=\"#\" class=\"right brand-logo\">Code Contas</a>\n                            <ul id=\"nav-mobile\" class=\"left\">\n                                <li v-for=\"o in menus\">\n                                    <a v-link=\"{name: o.routeName}\">{{ o.name }}</a>\n                                </li>\n                            </ul>\n                </div>\n            </nav>\n        </div>            \n        <router-view></router-view>\n        ",
     data: function data() {
         return {
-            menus: [{ name: "Contas a pagar", routeName: 'bill-pay.list' }, { name: "Contas a receber", routeName: 'bill-receive.list' }, { name: "DashBoard", routeName: 'dashboard' }]
+            menus: [{ name: "DashBoard", routeName: 'dashboard' }, { name: "Contas a pagar", routeName: 'bill-pay.list' }, { name: "Contas a receber", routeName: 'bill-receive.list' }]
         };
     }
 });
--- src/bill.component.js
+++ src/bill.component.js
@@ -1,20 +1,25 @@
 window.billComponent = Vue.extend({
     template: `
-        <nav>
-            <ul>
-                <li v-for="o in menus">
-                    <a v-link="{name: o.routeName}">{{ o.name }}</a>
-                </li>
-            </ul>
-        </nav>
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper container">
+                            <a href="#" class="right brand-logo">Code Contas</a>
+                            <ul id="nav-mobile" class="left">
+                                <li v-for="o in menus">
+                                    <a v-link="{name: o.routeName}">{{ o.name }}</a>
+                                </li>
+                            </ul>
+                </div>
+            </nav>
+        </div>            
         <router-view></router-view>
         `,
     data() {
         return {
             menus: [
+                {name: "DashBoard", routeName: 'dashboard'},
                 {name: "Contas a pagar", routeName: 'bill-pay.list'},
                 {name: "Contas a receber", routeName: 'bill-receive.list'},
-                {name: "DashBoard", routeName: 'dashboard'},
             ],
         };
     }
