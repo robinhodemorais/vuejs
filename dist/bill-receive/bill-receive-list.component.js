9e1b68e7b9643471f6291e92109f86d30e82a38e
robinhodemorais <robinhodemorais@gmail.com>
2016-10-23 00:49:01 -0200

    Excluindo Contas a Receber com o ModalComponent

--- dist/bill-receive/bill-receive-list.component.js
+++ dist/bill-receive/bill-receive-list.component.js
@@ -1,11 +1,18 @@
 'use strict';
 
 window.billReceiveListComponent = Vue.extend({
-    template: '\n                <div class="container">\n                    <div class="row">\n                    <h2>Minha contas á receber</h2>\n                        <table class="bordered striped highlight centered responsive-table">\n                            <thead>\n                            <tr>\n                                <th>#</th>\n                                <th>Vecimento</th>\n                                <th>Nome</th>\n                                <th>Valor</th>\n                                <th>Recebido?</th>\n                                <th>Ações</th>\n                            </tr>\n                            </thead>\n                            <tbody>\n                            <tr v-for="(index,o) in bills">\n                                <td>{{ index + 1 }}</td>\n                                <td>{{ o.date_due | dateFormat \'pt-BR\'}}</td>\n                                <td>{{ o.name | stringUpperCase}}</td>\n                                <td>{{ o.value | numberFormat \'pt-BR\'}}</td>\n                                <td class="white-text" :class="{\'green lighten-2\': o.done, \'red lighten-2\': !o.done}">\n                                    {{ o.done | doneLabelReceive }}\n                                </td>\n                                <td>\n                                    <a v-link="{ name: \'bill-receive.update\', params: {id: o.id} }">Editar</a> | \n                                    <a href="#" @click.prevent="deleteBill(o)">Excluir</a> \n                                </td>\n                            </tr>\n                            </tbody>\n                        </table>\n                    </div>\n                </div>                        \n    ',
+    components: {
+        'modal': modalComponent
+    },
+    template: '\n                <div class="container">\n                    <div class="row">\n                    <h2>Minha contas á receber</h2>\n                        <table class="bordered striped highlight centered responsive-table">\n                            <thead>\n                            <tr>\n                                <th>#</th>\n                                <th>Vecimento</th>\n                                <th>Nome</th>\n                                <th>Valor</th>\n                                <th>Recebido?</th>\n                                <th>Ações</th>\n                            </tr>\n                            </thead>\n                            <tbody>\n                            <tr v-for="(index,o) in bills">\n                                <td>{{ index + 1 }}</td>\n                                <td>{{ o.date_due | dateFormat \'pt-BR\'}}</td>\n                                <td>{{ o.name | stringUpperCase}}</td>\n                                <td>{{ o.value | numberFormat \'pt-BR\'}}</td>\n                                <td class="white-text" :class="{\'green lighten-2\': o.done, \'red lighten-2\': !o.done}">\n                                    {{ o.done | doneLabelReceive }}\n                                </td>\n                                <td>\n                                    <a v-link="{ name: \'bill-receive.update\', params: {id: o.id} }">Editar</a> | \n                                    <a href="#" @click.prevent="openModalDelete(o)">Excluir</a> \n                                </td>\n                            </tr>\n                            </tbody>\n                        </table>\n                    </div>\n                </div>   \n                <modal :modal="modal">\n                    <div slot="content">\n                        <h4>Mensagem de confirmação</h4>\n                        <p><strong>Deseja excluir essa conta ?</strong></p>\n                        <div class="divider"></div>\n                        <p>Nome: <strong>{{billToDelete.name}}</strong></p>\n                        <p>Valor: <strong>{{billToDelete.value | numberFormat}}</strong> </p>\n                        <p>Data de Vencimento: <strong>{{billToDelete.date_due | dateFormat }}</strong></p>\n                    </div>\n                    <div slot="footer">\n                        <button class="btn btn-flat waves-effect green lighten-2 modal-action modal-close" \n                                @click="deleteBill()">OK</button>\n                        <button class="btn btn-flat waves-effect red lighten-2 modal-action modal-close">Cancelar</button>\n                    </div>\n                </modal>                                     \n    ',
     data: function data() {
         return {
             //bills: this.$root.$children[0].billsPay
-            bills: []
+            bills: [],
+            billToDelete: null,
+            modal: {
+                id: 'modal-delete'
+            }
         };
     },
     created: function created() {
@@ -21,13 +28,18 @@ window.billReceiveListComponent = Vue.extend({
         deleteBill: function deleteBill(bill) {
             var _this2 = this;
 
-            if (confirm('Deseja excluir esta conta?')) {
-                // let self = this;
-                BillReceive.delete({ id: bill.id }).then(function (response) {
-                    _this2.bills.$remove(bill);
-                    _this2.$dispatch('change-info');
-                });
-            }
+            // if(confirm('Deseja excluir esta conta?')){
+            // let self = this;
+            BillReceive.delete({ id: this.billToDelete.id }).then(function (response) {
+                _this2.bills.$remove(_this2.billToDelete);
+                _this2.billToDelete = null;
+                _this2.$dispatch('change-info');
+            });
+            // }
+        },
+        openModalDelete: function openModalDelete(bill) {
+            this.billToDelete = bill;
+            $('#modal-delete').openModal();
         }
     }
 });
--- src/bill-receive/bill-receive-list.component.js
+++ src/bill-receive/bill-receive-list.component.js
@@ -1,4 +1,7 @@
 window.billReceiveListComponent = Vue.extend({
+    components:{
+        'modal': modalComponent
+    },
     template: `
                 <div class="container">
                     <div class="row">
@@ -25,18 +28,37 @@ window.billReceiveListComponent = Vue.extend({
                                 </td>
                                 <td>
                                     <a v-link="{ name: 'bill-receive.update', params: {id: o.id} }">Editar</a> | 
-                                    <a href="#" @click.prevent="deleteBill(o)">Excluir</a> 
+                                    <a href="#" @click.prevent="openModalDelete(o)">Excluir</a> 
                                 </td>
                             </tr>
                             </tbody>
                         </table>
                     </div>
-                </div>                        
+                </div>   
+                <modal :modal="modal">
+                    <div slot="content">
+                        <h4>Mensagem de confirmação</h4>
+                        <p><strong>Deseja excluir essa conta ?</strong></p>
+                        <div class="divider"></div>
+                        <p>Nome: <strong>{{billToDelete.name}}</strong></p>
+                        <p>Valor: <strong>{{billToDelete.value | numberFormat}}</strong> </p>
+                        <p>Data de Vencimento: <strong>{{billToDelete.date_due | dateFormat }}</strong></p>
+                    </div>
+                    <div slot="footer">
+                        <button class="btn btn-flat waves-effect green lighten-2 modal-action modal-close" 
+                                @click="deleteBill()">OK</button>
+                        <button class="btn btn-flat waves-effect red lighten-2 modal-action modal-close">Cancelar</button>
+                    </div>
+                </modal>                                     
     `,
     data() {
         return {
             //bills: this.$root.$children[0].billsPay
-            bills: []
+            bills: [],
+            billToDelete: null,
+            modal: {
+                id: 'modal-delete'
+            }
         };
     },
     created(){
@@ -47,13 +69,18 @@ window.billReceiveListComponent = Vue.extend({
     },
     methods: {
         deleteBill(bill) {
-            if(confirm('Deseja excluir esta conta?')){
+           // if(confirm('Deseja excluir esta conta?')){
                // let self = this;
-                BillReceive.delete({id: bill.id}).then((response) => {
-                    this.bills.$remove(bill);
+                BillReceive.delete({id: this.billToDelete.id}).then((response) => {
+                    this.bills.$remove(this.billToDelete);
+                    this.billToDelete = null;
                     this.$dispatch('change-info');
                 });
-            }
+           // }
+        },
+        openModalDelete(bill){
+            this.billToDelete = bill;
+            $('#modal-delete').openModal();
         }
     }
 });
