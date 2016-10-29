f0e4b1c90f67e5fbd55272a704bd5e693d9b4e1a
robinhodemorais <robinhodemorais@gmail.com>
2016-10-29 15:00:54 -0200

    Babel-loader

--- webpack/main.js
+++ webpack/main.js
@@ -1,11 +1,12 @@
 //com amdjs
-require(['./clients', './funcao', './variables'], function (colecao,minhaFuncao,minhaVariavel) {
+require(['./clients', './funcao'], function (colecao,minhaFuncao) {
     //em colecao pode ser qualquer nome
     console.log(colecao);
 
     console.log(minhaFuncao(10,20));
 
-    console.log(minhaVariavel);
+    //console.log(minhaVariavel);
+    console.log(require('./variables'));
 });
 
 
--- webpack/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: './main.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'app.bundle.js',
-        publicPath: '/dist/'
-    }
-};
